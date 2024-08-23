46744343521fe83d42ca4f12ead2684f23d820b3
sauravphuyal2001 <sauravphuyal2001@gmail.com>
2024-08-23 19:32:37 +0545

    Day 7

--- app.js
+++ app.js
@@ -1,58 +1,38 @@
 // const app = require('express')()
 const express = require('express')
 const app = express()
+require('dotenv').config()
+
+const { blogs } = require('./model/index');
+
 
 app.set('view engine', 'ejs')
+require("./model/index")
 
-app.get('/', (req, res) => {
-    const testimonals = {
-        name: 'Manish Banset',
-        message: "Manish Basnet sent a message: 'KHATRA DEVELOPER BANNU HAI'"
-    }
-    const data = {
-        name: "Saurav Phuyal",
-        age: 23,
-        location: 'Gachhiya'
-    }
-    const nepal = {
-        continent: 'Asia',
-    }
-    const kura = {
-        desc: 'Am I MERN DEVELOPER?',
-    }
-    res.render("home.ejs", {
-        haha: data,
-        hehe: nepal,
-        huhu: testimonals,
-        hora: kura
+//app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+app.get("/create", (req, res)=>{
+    res.render('create')
+})
+
+app.post('/create',async (req, res) => {
+//    const title = req.body.title
+//    const subtitle =  req.body.subtile
+//     const description = req.body.description
+    const {title, subtitle, description} = req.body
+    await blogs.create({
+        title : title,
+        subtitle : subtitle,
+        description : description
     })
-})
 
-app.get('/about', (req, res) => {
-    res.render("test/about")
-})
-
-app.get('/blog', (req, res) => {
-    const kura = {
-        desc: 'Am I MERN DEVELOPER?',
-    }
-    res.render("blog.ejs", {
-        hora: kura
-    })
-})
-app.get('/timepass', (req, res) => {
-    const bored = {
-        manishlaivaneko: 'Oe, Xutti Vayo vanna!',
-    }
-    res.render("timepass.ejs", {
-        hahahe: bored
-    })
-})
-
+    res.send('Blogs Added Successfully'); // Send a response back to the client
+});
 
 app.use(express.static('public/css/'))
 
-app.listen(3306, () => {
+app.listen(3000, () => {
     console.log('server is running on port 3000')
 })
 
--- model/index.js
+++ model/index.js
@@ -1,7 +1,38 @@
-const {Sequelize, DataTypes} = require('sequlize')
+const {Sequelize,DataTypes} = require('sequelize')
+const databaseConfig = require('../config/dbConfig')
+const makeBlogTable = require('./blogModel')
+const makeUserTable = require('./userModel')
 
-const sequlize = new Sequelize ('haha', 'root', '', {
-    host: 'localhost',
-    port: 3306
 
-})  //database
+const sequelize = new Sequelize(databaseConfig.db,databaseConfig.username,databaseConfig.password,{
+    host : databaseConfig.host, 
+    port : databaseConfig.port, 
+    dialect : databaseConfig.dialect, 
+    operatorsAliases : false, 
+    pool : {
+        max : 5, 
+        min : 0, 
+        acquire : 30000,
+        idle : 10000
+    }
+})
+
+sequelize.authenticate()
+.then(()=>{
+    console.log("The Username & Password is Correct")
+})
+.catch((err)=>{
+    console.log("The Username & Password is Incorrect",err)
+})
+
+const db = {}
+db.Sequelize = Sequelize 
+db.sequelize = sequelize
+
+db.blogs = makeBlogTable(sequelize, DataTypes)
+db.users = makeUserTable(sequelize, DataTypes)
+
+db.sequelize.sync({force : false}).then(()=>{  //to rename/add database table make it true,
+    console.log("Synced done!!")
+})
+module.exports = db 
