04e536e57143b8309b5755a17c7f76b931faa56d
sauravphuyal2001 <sauravphuyal2001@gmail.com>
2024-08-29 19:22:07 +0545

    Day 10

--- app.js
+++ app.js
@@ -1,45 +1,49 @@
-// const app = require('express')()
-const express = require('express')
-const app = express()
 require('dotenv').config()
+const express = require('express')
+const { blogs, sequelize } = require('./model/index')
+const {multer, storage} = require('./middleware/multerConfig')
+// const {multer, storage, storage2, storage3} = require('./middleware/multerConfig')
+const upload = multer({ storage: storage }) // for blog images
+// const uploadBanner = multer({ storage: storage2 }) // for banner images
+// const uploadUser = multer({ storage: storage3 }) // for user images
 
-const { blogs, sequelize } = require('./model/index');
-
+const app = express()
 
 app.set('view engine', 'ejs')
 require("./model/index")
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-//app.use(express.json())
-app.use(express.urlencoded({extended : true}))
-
-app.get("/create", (req, res)=>{
-    res.render('create')
-})
-app.get("/create", (req, res)=>{
-    res.render('create')
+app.get("/create", (req, res) => {
+    res.render("create")
 })
 
-app.post('/create',async (req, res) => {
-//    const title = req.body.title
-//    const subtitle =  req.body.subtile
-//     const description = req.body.description
-    const {title, subtitle, description} = req.body
-
-    // "INSERT INTO blogs (title, subtitle, description) values ('$title', '$subtitle', $description)"
-    // sequelize.query("INSERT INTO blogs (title, subtitle, description) values ('$title', '$subtitle', $description)")
+// Upload blog image
+app.post('/create', upload.single('image'), async (req, res) => {
+    const { title, subtitle, description } = req.body
 
     await blogs.create({
-        title : title,
-        subtitle : subtitle,
-        description : description
+        title: title,
+        subtitle: subtitle,
+        description: description,
+        image: req.file.filename // save the image filename to the database
     })
 
-    res.send('Blogs Added Successfully'); // Send a response back to the client
+    res.send('Blog Added Successfully');
 });
+
+// // Upload banner image
+// app.post('/upload-banner', uploadBanner.single('banner'), (req, res) => {
+//     res.send(`Banner uploaded successfully to ${req.file.path}`);
+// });
+
+// // Upload user image
+// app.post('/upload-user', uploadUser.single('user'), (req, res) => {
+//     res.send(`User image uploaded successfully to ${req.file.path}`);
+// });
 
 app.use(express.static('public/css/'))
 
 app.listen(3000, () => {
     console.log('server is running on port 3000')
 })
-
--- middleware/multerConfig.js
+++ middleware/multerConfig.js
@@ -1,9 +1,31 @@
-const multer  = require('multer')
+const multer = require('multer');
 
 const storage = multer.diskStorage({
-    destination : function(req, file, cb){
-        cb(null, './storage')
-
+    destination: (req, file, cb) => {
+        cb(null, './storage');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
     }
-})
-module.exports = {storage}
+});
+
+// const storage2 = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, './storage/banner');
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, Date.now() + '-' + file.originalname);
+//     }
+// });
+
+// const storage3 = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, './storage/user');
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, Date.now() + '-' + file.originalname);
+//     }
+// });
+
+module.exports = { multer, storage };
+// module.exports = { multer, storage, storage2, storage3 };
