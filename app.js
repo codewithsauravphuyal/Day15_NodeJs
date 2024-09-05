3d426b4c9c29399bbdf6c211c7d50df27162b244
sauravphuyal2001 <sauravphuyal2001@gmail.com>
2024-09-05 19:55:09 +0545

    day 15

--- app.js
+++ app.js
@@ -1,36 +1,27 @@
 require('dotenv').config()
 const express = require('express')
 const { blogs, sequelize, users } = require('./model/index')
-const { multer, storage } = require('./middleware/multerConfig')
-// const {multer, storage, storage2, storage3} = require('./middleware/multerConfig')
-const upload = multer({ storage: storage }) // for blog images
-// const uploadBanner = multer({ storage: storage2 }) // for banner images
-// const uploadUser = multer({ storage: storage3 }) // for user images
 const bcrypt = require("bcrypt")
 const { homePage, singleBlog, deleteBlog, createForm, createBlog } = require('./controller/blogController')
 const { registerUser, loginUser, renderRegister, renderLogin } = require('./controller/authController')
 
 const app = express()
+const blogRoute = require("./routes/blogRoute")
+const authRoute = require("./routes/authRoute")
 
 app.set('view engine', 'ejs')
 require("./model/index")
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.set('view engine', 'ejs')
-require("./model/index")
-app.use(express.urlencoded({ extended: true }))
-
-app.get("/", homePage)
-app.get("/blog/:id", singleBlog)
-app.get("/delete/:id", deleteBlog)
-app.get("/create", createForm)
-app.post('/create', upload.single('image'), createBlog)
-
-app.get("/register", renderRegister)
-app.post("/register", registerUser)
-app.get("/login", renderLogin)
-app.post("/login", loginUser)
+//http://localhost:3000/ + /
+//http://localhost:3000/ + /blog/:id
+//http://localhost:3000/ + /delete/:id
+//http://localhost:3000/ + /create
+app.use('/', blogRoute)
+app.use('/', authRoute)
+// http://localhost:3000 + register
+// http://localhost:3000 + login
 
 
 
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,12 +1,16 @@
 const { homePage, createForm, createBlog, singleBlog, deleteBlog } = require('../controller/blogController')
 
 const router = require('express').Router()
-
+const { multer, storage } = require('../middleware/multerConfig')
+// const {multer, storage, storage2, storage3} = require('./middleware/multerConfig')
+const upload = multer({ storage: storage }) // for blog images
+// const uploadBanner = multer({ storage: storage2 }) // for banner images
+// const uploadUser = multer({ storage: storage3 }) // for user images
 
 router.route("/").get(homePage)
 router.route("/blog/:id").get(singleBlog)
 router.route("/delete/:id").get(deleteBlog)
-router.route("/create").get(createForm).post(createBlog)
+router.route("/create").get(createForm).post(upload.single('image'), createBlog)
 
 
 
